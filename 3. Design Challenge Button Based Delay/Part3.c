e1478ac8bc7d6fd25ed5463f39810483e6b067c8
gerace13 <gerace13@students.rowan.edu>
2023-02-22 13:03:05 -0500

    finalized P3

--- 3. Design Challenge Button Based Delay/Part3.c
+++ 3. Design Challenge Button Based Delay/Part3.c
@@ -2,7 +2,7 @@
  * Part3.c
  *
  *  Created on: Feb 12, 2023
- *      Author: FILL THIS IN
+ *      Author: Matthew Gerace
  *
  *      YOU NEED TO FILL IN THIS AUTHOR BLOCK
  */
@@ -12,7 +12,6 @@
 
 void init();
 void reset();
-void updateCount();
 int count = 0;
 int cycles = 0;
 int times = 0;
@@ -21,14 +20,14 @@ void main()
     WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
     PM5CTL0 &= ~LOCKLPM5;
 
-    init();
-    reset();
+    init(); //call init func
+    reset(); //call reset func
     __bis_SR_register(LPM3_bits | GIE);
 
     char button = gpioRead(2, 3); //make button sensitive to P2.3
     while (!button)
     {
-        times = times + 2400;
+        times = times + 2400; //increment times
         __delay_cycles(100000);  //delay for 0.1s
     }
 
@@ -37,10 +36,10 @@ void main()
 
 void init ()
 {
-    gpioInit(1, 0, 1);
-    gpioInit(6, 6, 1);
-    gpioInit(2, 3, 0);
-    gpioInit(4, 1, 0);
+    gpioInit(1, 0, 1); //initialize P1.0 to output
+    gpioInit(6, 6, 1); //initialize P6.6 to output
+    gpioInit(2, 3, 0); //initialize p2.3 to input
+    gpioInit(4, 1, 0); //initialize p4.1 to input
 
     P4REN |= BIT1;               // Enable Resistor on P4.1
     P4OUT |= BIT1;               // Configure Resistor on P4.1 to be Pullup
@@ -49,16 +48,16 @@ void init ()
 
     P2IES &= ~BIT3;                         // P2.3 Low --> High edge    (changed the edge sensitivity)
     P2IE |= BIT3;                           // P2.3 interrupt enabled
-    P4IES &= ~BIT1;
-    P4IE |= BIT1;
+    P4IES &= ~BIT1; //edge sensitivity
+    P4IE |= BIT1; //enable interrupt
 
 }
 
 void reset ()
 {
-    count = 24000;
-    TB1CCTL0 = CCIE;
-    TB1CCR0 == count;
+    count = 24000; //set starting count
+    TB1CCTL0 = CCIE; //enable interrupt
+    TB1CCR0 == count; //set TB1 to count
     TB1CTL = TBSSEL_1 | MC_2;
 
 }
@@ -67,17 +66,17 @@ void reset ()
 __interrupt void Timer1_B0_ISR(void)
 {
     // @TODO You can toggle the LED Pin in this routine and if adjust your count in CCR0.
-    P6OUT ^= BIT6;
-    TB1CCR0 += count;
+    P6OUT ^= BIT6; //toggle p6
+    TB1CCR0 += count; //add count
 
 }
 
 #pragma vector=PORT2_VECTOR
 __interrupt void Port_2(void)
 {
-    P2IFG &= !BIT3;
-    count = times;
-    times = 0;
+    P2IFG &= !BIT3; //disable interrupt flag
+    count = times; //set count = to times
+    times = 0; //reset times
 
 
 }
@@ -86,7 +85,7 @@ __interrupt void Port_2(void)
 #pragma vector=PORT4_VECTOR
 __interrupt void Port_4(void)
 {
-    P4IFG &= ~BIT1;
-    reset();
+    P4IFG &= ~BIT1; //reset interrupt flag
+    reset(); //call reset
 
 }
